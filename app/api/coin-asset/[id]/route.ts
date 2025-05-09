b8870c35b5cec0014c486d05b05cc3d1b842e14d
Arjan Mitra <arjanmitra@mac.mynetworksettings.com>
2025-05-08 22:29:08 -0400

    fix

--- app/api/coin-asset/[id]/route.ts
+++ app/api/coin-asset/[id]/route.ts
@@ -1,7 +1,7 @@
 import { CoinCapAssetResponse } from '@/app/types/coincap'
 import { NextResponse } from 'next/server'
 
-export const GET = async (_req: Request, context: { params: { id: string } }) => {
+export const GET = async (_req: Request, context: any) => {
     const { id } = await context.params
     const res = await fetch(`https://rest.coincap.io/v3/assets/${id}`, {
         headers: { Authorization: `Bearer ${process.env.COINCAP_API_KEY}` }
